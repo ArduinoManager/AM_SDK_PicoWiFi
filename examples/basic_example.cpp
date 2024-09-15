dd349a4de9a3755337630f3f3482e66a704c9ed2
Fabrizio Boco <fabrizio.boco@gmail.com>
2024-09-15 17:26:38 +0200

    Disclamers

--- examples/basic_example.cpp
+++ examples/basic_example.cpp
@@ -1,3 +1,34 @@
+/*
+   Test Arduino Manager for iPad / iPhone / Mac
+
+   A simple test program to show the Arduino Manager features.
+
+   Author: Fabrizio Boco - fabboco@gmail.com
+
+   Version: 1.0
+
+   09/15/2024
+
+   All rights reserved
+
+*/
+
+/*
+   AMController libraries, example sketches (The Software) and the related documentation (The Documentation) are supplied to you
+   by the Author in consideration of your agreement to the following terms, and your use or installation of The Software and the use of The Documentation
+   constitutes acceptance of these terms.
+   If you do not agree with these terms, please do not use or install The Software.
+   The Author grants you a personal, non-exclusive license, under authors copyrights in this original software, to use The Software.
+   Except as expressly stated in this notice, no other rights or licenses, express or implied, are granted by the Author, including but not limited to any
+   patent rights that may be infringed by your derivative works or by other works in which The Software may be incorporated.
+   The Software and the Documentation are provided by the Author on an AS IS basis.  THE AUTHOR MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT
+   LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE SOFTWARE OR ITS USE AND OPERATION
+   ALONE OR IN COMBINATION WITH YOUR PRODUCTS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL DAMAGES (INCLUDING,
+   BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE,
+   REPRODUCTION AND MODIFICATION OF THE SOFTWARE AND OR OF THE DOCUMENTATION, HOWEVER CAUSED AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
+   STRICT LIABILITY OR OTHERWISE, EVEN IF THE AUTHOR HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
 #include <stdio.h>
 #include "pico/stdlib.h"
 
--- include/AM_SDK_PicoWiFi.h
+++ include/AM_SDK_PicoWiFi.h
@@ -1,3 +1,26 @@
+/*
+
+   AMController libraries, example sketches (“The Software”) and the related documentation (“The Documentation”) are supplied to you
+   by the Author in consideration of your agreement to the following terms, and your use or installation of The Software and the use of The Documentation
+   constitutes acceptance of these terms.
+   If you do not agree with these terms, please do not use or install The Software.
+   The Author grants you a personal, non-exclusive license, under author's copyrights in this original software, to use The Software.
+   Except as expressly stated in this notice, no other rights or licenses, express or implied, are granted by the Author, including but not limited to any
+   patent rights that may be infringed by your derivative works or by other works in which The Software may be incorporated.
+   The Software and the Documentation are provided by the Author on an "AS IS" basis.  THE AUTHOR MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT
+   LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE SOFTWARE OR ITS USE AND OPERATION
+   ALONE OR IN COMBINATION WITH YOUR PRODUCTS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL DAMAGES (INCLUDING,
+   BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE,
+   REPRODUCTION AND MODIFICATION OF THE SOFTWARE AND OR OF THE DOCUMENTATION, HOWEVER CAUSED AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
+   STRICT LIABILITY OR OTHERWISE, EVEN IF THE AUTHOR HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+   Author: Fabrizio Boco - fabboco@gmail.com
+
+   Version: 1.0
+
+   All rights reserved
+
+*/
 #ifndef AM_PICOWIFI_H
 #define AM_PICOWIFI_H
 
