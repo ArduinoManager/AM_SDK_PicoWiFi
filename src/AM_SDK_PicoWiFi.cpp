c338cf407bfb7c13473ae1799fd23b529477fc23
Fabrizio Boco <fabrizio.boco@gmail.com>
2026-05-17 09:36:21 +0200

    Fixed ADC reading-to-voltage conversion formula

--- src/AM_SDK_PicoWiFi.cpp
+++ src/AM_SDK_PicoWiFi.cpp
@@ -500,7 +500,7 @@ void AMController::gpio_temporary_put(uint pin, bool value, uint ms)
 
 float AMController::to_voltage(uint16_t adc_value, float vref)
 {
-   const float conversion_factor = vref / (1 << 12);
+   const float conversion_factor = vref / ((1 << 12) - 1);
    return adc_value * conversion_factor;
 }
 
